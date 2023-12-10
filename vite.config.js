759d5b0344e73393390b3165b795c245d2914fc5
Tokle89 <fredriktokle@gmail.com>
2023-12-10 10:49:55 +0100

    updates vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-export default {
+/* eslint-disable no-undef */
+import { resolve } from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
   server: {
     port: 8080,
     hot: true,
   },
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "auth/login/index.html"),
+        register: resolve(__dirname, "auth/register/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+        listing: resolve(__dirname, "listing/index.html"),
+      },
+    },
+  },
+});
